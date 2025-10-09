9d59ef8c2ba2bc86f0d9049e9e2b42a23be99b7e
S-Ketan <ketan10923@gmail.com>
2025-10-09 14:23:57 +0200

    Add AuthController, LoginRequest, and SignupRequest for user authentication

---
+++ backend/app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Foundation\Auth\User as AuthUser;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ], 422);
+        }
+
+        /**  @var \App\Models\User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+
+        ]);
+
+
+    }
+    public function logout(Request $request)
+    {
+        $user = $request->user();
+        return response()->json([
+            'message' => 'Logged out'
+        ], 204);
+    }
+    public function Signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+
+        ]);
+    }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ backend/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\api\AuthController;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/signup', [AuthController::class, 'signup']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
